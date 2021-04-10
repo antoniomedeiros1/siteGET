598c02376b468cdc52748ec9eb7a8ee8b84f6bd1
Alexandre Vitor <alexandre.braga@estudante.ufjf.br>
2021-04-10 18:45:25 -0300

    conserta link yt

--- src/pages/curso.js
+++ src/pages/curso.js
@@ -9,13 +9,13 @@ const about = () => {
             <div className="container-main">
                 <iframe
                     style={{display: "block", margin: "auto"}}
-                    width="853" 
-                    height="505" 
-                    src="https://www.youtube.com/watch?v=50CXmj47NKE" 
-                    title="YouTube video player" 
-                    frameborder="0" 
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                    allowfullscreen="true"
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/50CXmj47NKE"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
                 >
                 </iframe>
                 <div className="text">
