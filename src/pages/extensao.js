47797111187780ddc8d5e88938ffaa84bf30b329
Antonio <antoniojmf2020@outlook.com>
2021-04-18 10:22:03 -0300

    diminui a largura do text e mudei a descricao da pagina de extensao

--- src/pages/extensao.js
+++ src/pages/extensao.js
@@ -14,10 +14,21 @@ const extensao = () => {
         </div>
         <div className="container-main">
             <div className='text'>
+                <div className="title1">
+                    Extensão 👥
+                </div>
                 <div className="paragrafo">
                     Nas atividades de extensão o grupo visa levar 
                     para a comunidade externa o conhecimento adquirido
-                    ao longo da graduação.
+                    ao longo da graduação. Partindo dessa proposta, o grupo
+                    desenvolve uma série de projetos em parcerias com escolas públicas
+                    e organizações educacionais, oferecendo cursos de tecnologias
+                    relacionadas com a Engenharia Computacional,
+                    importantes para o mercado de trabalho e para o ensino, 
+                    tendo como objetivo secundário a divulgação do curso.
+                </div>
+                <div className="paragrafo">
+                    Atualmente, o grupo trabalha nos seguintes projetos de extensão:
                 </div>
             </div>
             <ProjetosExtensao />
