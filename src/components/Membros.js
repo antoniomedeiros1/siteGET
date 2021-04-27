8c543ab634e901192ae6603e04c0414544c6943a
caiocrocha <caiocedrola@ice.ufjf.br>
2021-04-27 20:19:57 -0300

    Add info dos membros

--- src/components/Membros.js
+++ src/components/Membros.js
@@ -9,7 +9,7 @@ const Membros = () => {
             <div className="m-5 bd-highlight col">
                 <div className="container-col">
                     <div className="membro">
-                        <img src="https://images.generated.photos/AYrgR1kiUKsr9Mjohj9Yy8K5LzXNZmvkBlmiU4kNKZ4/rs:fit:256:256/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA4NzIzNDguanBn.jpg" alt="Elson" class="fotoMembro"></img>
+                        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" alt="Elson" class="fotoMembro"></img>
                         <div className="redesSociaisMembro">
                             <a href="https://instagram.com" target="_blank" rel="noreferrer"><img src={Instagram} alt="Instagram"></img></a>
                             <a href="https://twitter.com/" target="_blank" rel="noreferrer"><img src={Twitter} alt="Twitter"></img></a>
@@ -17,16 +17,18 @@ const Membros = () => {
                     </div>
                     <div className="infoMembro">
                         <div className="nomeMembro"><h1>Elson Magalhães Toledo</h1>
-                            <span class="textoMembro">Entrou em ?</span>
+                            <span class="textoMembro">
+                                Tutor<br />
+                                Entrou em ?
+                            </span>
                         </div>
-                        <h2>Tutor</h2>
                     </div>
                 </div>
             </div>
             <div className="m-5 bd-highlight col">
                 <div className="container-col">
                     <div className="membro">
-                        <img src="https://www.researchgate.net/profile/Shengfeng-He-2/publication/342544514/figure/fig2/AS:907947169284104@1593482882207/Multi-view-synthesis-results-on-CelebA-For-each-case-we-show-the-results-of-ours.ppm" alt="Alexandre" class="fotoMembro"></img>
+                        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" alt="Alexandre" class="fotoMembro"></img>
                         <div className="redesSociaisMembro">
                             <a href="https://instagram.com" target="_blank" rel="noreferrer"><img src={Instagram} alt="Instagram"></img></a>
                             <a href="https://twitter.com/" target="_blank" rel="noreferrer"><img src={Twitter} alt="Twitter"></img></a>
@@ -34,16 +36,18 @@ const Membros = () => {
                     </div>
                     <div className="infoMembro">
                         <div className="nomeMembro"><h1>Alexandre Vitor Silva Braga</h1>
-                            <span class="textoMembro">Entrou em 2019</span>
+                            <span class="textoMembro">
+                                Bolsista<br />
+                                Entrou em 2019
+                            </span>
                         </div>
-                        <h2>Membro</h2>
                     </div>
                 </div>
             </div>
             <div className="m-5 bd-highlight col">
                 <div className="container-col">
                     <div className="membro">
-                        <img src="https://images.generated.photos/fCi46s8iQVIrOsBYXIhjenJuDqC-Bms9tnHuS9mv3Xs/rs:fit:256:256/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA4MzM4MDkuanBn.jpg" alt="Ana" class="fotoMembro"></img>
+                        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" alt="Ana" class="fotoMembro"></img>
                         <div className="redesSociaisMembro">
                             <a href="https://instagram.com" target="_blank" rel="noreferrer"><img src={Instagram} alt="Instagram"></img></a>
                             <a href="https://twitter.com/" target="_blank" rel="noreferrer"><img src={Twitter} alt="Twitter"></img></a>
@@ -51,16 +55,18 @@ const Membros = () => {
                     </div>
                     <div className="infoMembro">
                         <div className="nomeMembro"><h1>Ana Carolina Guedes de Oliveira Vieira</h1>
-                            <span class="textoMembro">Entrou em 2019</span>
+                            <span class="textoMembro">
+                                Bolsista<br />
+                                Entrou em 2019
+                            </span>
                         </div>
-                        <h2>Membro</h2>
                     </div>
                 </div>
             </div>                    
             <div className="m-5 bd-highlight col">
                 <div className="container-col">
                     <div className="membro">
-                        <img src="https://images.generated.photos/-VjJ0YC4EAcOw2fbNNc6NEowWeLH_x541cCWzW8q4CY/rs:fit:256:256/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA0MDIxNzUuanBn.jpg" alt="Antonio" class="fotoMembro"></img>
+                        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" alt="Antonio" class="fotoMembro"></img>
                         <div className="redesSociaisMembro">
                             <a href="https://instagram.com" target="_blank" rel="noreferrer"><img src={Instagram} alt="Instagram"></img></a>
                             <a href="https://twitter.com/" target="_blank" rel="noreferrer"><img src={Twitter} alt="Twitter"></img></a>
@@ -68,16 +74,18 @@ const Membros = () => {
                     </div>
                     <div className="infoMembro">
                         <div className="nomeMembro"><h1>Antônio José de Medeiros Filho</h1>
-                            <span class="textoMembro">Entrou em 2019</span>
+                            <span class="textoMembro">
+                                Bolsista<br />
+                                Entrou em 2019
+                            </span>
                         </div>
-                        <h2>Membro</h2>
                     </div>
                 </div>
             </div>
             <div className="m-5 bd-highlight col">
                 <div className="container-col">
                     <div className="membro">
-                        <img src="https://images.generated.photos/hpYCpOZejY8yesySnqo1kcO655x6EtMmDci_vot_5LA/rs:fit:256:256/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzAwNjQ2NjQuanBn.jpg" alt="Caio" class="fotoMembro"></img>
+                        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" alt="Caio" class="fotoMembro"></img>
                         <div className="redesSociaisMembro">
                             <a href="https://instagram.com" target="_blank" rel="noreferrer"><img src={Instagram} alt="Instagram"></img></a>
                             <a href="https://twitter.com/" target="_blank" rel="noreferrer"><img src={Twitter} alt="Twitter"></img></a>
@@ -85,16 +93,18 @@ const Membros = () => {
                     </div>
                     <div className="infoMembro">
                         <div className="nomeMembro"><h1>Caio Cedrola Rocha</h1>
-                            <span class="textoMembro">Entrou em 2019</span>
+                            <span class="textoMembro">
+                                Bolsista<br />
+                                Entrou em 2019
+                            </span>
                         </div>
-                        <h2>Membro</h2>
                     </div>
                 </div>
             </div>                    
             <div className="m-5 bd-highlight col">
                 <div className="container-col">
                     <div className="membro">
-                        <img src="https://images.generated.photos/a_bA_5ZrKgXyjs4LR6IFcnrt88isnCnk1guSRntRyJg/rs:fit:256:256/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA4MTI5MjUuanBn.jpg" alt="Daniel" class="fotoMembro"></img>
+                        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" alt="Daniel" class="fotoMembro"></img>
                         <div className="redesSociaisMembro">
                             <a href="https://instagram.com" target="_blank" rel="noreferrer"><img src={Instagram} alt="Instagram"></img></a>
                             <a href="https://twitter.com/" target="_blank" rel="noreferrer"><img src={Twitter} alt="Twitter"></img></a>
@@ -102,16 +112,18 @@ const Membros = () => {
                     </div>
                     <div className="infoMembro">
                         <div className="nomeMembro"><h1>Daniel Souza Ferreira</h1>
-                            <span class="textoMembro">Entrou em ?</span>
+                            <span class="textoMembro">
+                                Bolsista<br />
+                                Entrou em ?
+                            </span>
                         </div>
-                        <h2>Membro</h2>
                     </div>
                 </div>
             </div>
             <div className="m-5 bd-highlight col">
                 <div className="container-col">
                     <div className="membro">
-                        <img src="https://images.generated.photos/pRMYklNK-nAbCUDBnmoKVVb4Mde7pOTzDj0VjhN2Us8/rs:fit:256:256/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzAyMzUxMDEuanBn.jpg" alt="Gabriel" class="fotoMembro"></img>
+                        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" alt="Gabriel" class="fotoMembro"></img>
                         <div className="redesSociaisMembro">
                             <a href="https://instagram.com" target="_blank" rel="noreferrer"><img src={Instagram} alt="Instagram"></img></a>
                             <a href="https://twitter.com/" target="_blank" rel="noreferrer"><img src={Twitter} alt="Twitter"></img></a>
@@ -119,16 +131,18 @@ const Membros = () => {
                     </div>
                     <div className="infoMembro">
                         <div className="nomeMembro"><h1>Gabriel Maciel Furlong</h1>
-                            <span class="textoMembro">Entrou em 2021</span>
+                            <span class="textoMembro">
+                                Voluntário<br />
+                                Entrou em 2021
+                            </span>
                         </div>
-                        <h2>Membro</h2>
                     </div>
                 </div>
             </div>
             <div className="m-5 bd-highlight col">
                 <div className="container-col">
                     <div className="membro">
-                        <img src="https://images.generated.photos/rUJssnBZcheHFQ4zO2haoTcY748tIO-MCmZmwLT8umo/rs:fit:256:256/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA3MDA4MTIuanBn.jpg" alt="Gabriele" class="fotoMembro"></img>
+                        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" alt="Gabriele" class="fotoMembro"></img>
                         <div className="redesSociaisMembro">
                             <a href="https://instagram.com" target="_blank" rel="noreferrer"><img src={Instagram} alt="Instagram"></img></a>
                             <a href="https://twitter.com/" target="_blank" rel="noreferrer"><img src={Twitter} alt="Twitter"></img></a>
@@ -136,16 +150,18 @@ const Membros = () => {
                     </div>
                     <div className="infoMembro">
                         <div className="nomeMembro"><h1>Gabriele Cesar Iwashima</h1>
-                            <span class="textoMembro">Entrou em ?</span>
+                            <span class="textoMembro">
+                                Bolsista<br />
+                                Entrou em ?
+                            </span>
                         </div>
-                        <h2>Membro</h2>
                     </div>
                 </div>
             </div>
             <div className="m-5 bd-highlight col">
                 <div className="container-col">
                     <div className="membro">
-                        <img src="https://images.generated.photos/q5W2gR9Ew39TJVc3UPoDMUrFuDslNDUCvOLKZ29qtzc/rs:fit:256:256/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA0NTUxNzkuanBn.jpg" alt="JoaoS" class="fotoMembro"></img>
+                        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" alt="JoaoS" class="fotoMembro"></img>
                         <div className="redesSociaisMembro">
                             <a href="https://instagram.com" target="_blank" rel="noreferrer"><img src={Instagram} alt="Instagram"></img></a>
                             <a href="https://twitter.com/" target="_blank" rel="noreferrer"><img src={Twitter} alt="Twitter"></img></a>
@@ -153,16 +169,18 @@ const Membros = () => {
                     </div>
                     <div className="infoMembro">
                         <div className="nomeMembro"><h1>João Stephan Silva Maurício</h1>
-                            <span class="textoMembro">Entrou em 2021</span>
+                            <span class="textoMembro">
+                                Voluntário<br />
+                                Entrou em 2021
+                            </span>
                         </div>
-                        <h2>Membro</h2>
                     </div>
                 </div>
             </div>
             <div className="m-5 bd-highlight col">
                 <div className="container-col">
                     <div className="membro">
-                        <img src="https://images.generated.photos/1mWCLnWf1h6H1C9fiGRMCf2ILt-XGpYBZqHObbDcjSo/rs:fit:256:256/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA2MjQ4MzQuanBn.jpg" alt="JoaoV" class="fotoMembro"></img>
+                        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" alt="JoaoV" class="fotoMembro"></img>
                         <div className="redesSociaisMembro">
                             <a href="https://instagram.com" target="_blank" rel="noreferrer"><img src={Instagram} alt="Instagram"></img></a>
                             <a href="https://twitter.com/" target="_blank" rel="noreferrer"><img src={Twitter} alt="Twitter"></img></a>
@@ -170,16 +188,18 @@ const Membros = () => {
                     </div>
                     <div className="infoMembro">
                         <div className="nomeMembro"><h1>João Víctor Costa de Oliveira</h1>
-                            <span class="textoMembro">Entrou em 2021</span>
+                            <span class="textoMembro">
+                                Bolsista<br />
+                                Entrou em 2021
+                            </span>
                         </div>
-                        <h2>Membro</h2>
                     </div>
                 </div>
             </div>
             <div className="m-5 bd-highlight col">
                 <div className="container-col">
                     <div className="membro">
-                        <img src="https://images.generated.photos/cRYh5QhwTx0svvuIm7pN7doX94pw_mVSkTN9oBYTzAA/rs:fit:256:256/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzAyMzg0OTIuanBn.jpg" alt="Lucas" class="fotoMembro"></img>
+                        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" alt="Lucas" class="fotoMembro"></img>
                         <div className="redesSociaisMembro">
                             <a href="https://instagram.com" target="_blank" rel="noreferrer"><img src={Instagram} alt="Instagram"></img></a>
                             <a href="https://twitter.com/" target="_blank" rel="noreferrer"><img src={Twitter} alt="Twitter"></img></a>
@@ -187,16 +207,18 @@ const Membros = () => {
                     </div>
                     <div className="infoMembro">
                         <div className="nomeMembro"><h1>Lucas Augusto Müller de Souza</h1>
-                            <span class="textoMembro">Entrou em ?</span>
+                            <span class="textoMembro">
+                                Bolsista<br />
+                                Entrou em ?
+                            </span>
                         </div>
-                        <h2>Membro</h2>
                     </div>
                 </div>
             </div>
             <div className="m-5 bd-highlight col">
                 <div className="container-col">
                     <div className="membro">
-                        <img src="https://images.generated.photos/YDTckkCczOwD60oQUIpxJUzXi7rVmQ5DLJBfOg-XHUM/rs:fit:256:256/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA3NzYzNzkuanBn.jpg" alt="Marcelo" class="fotoMembro"></img>
+                        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" alt="Marcelo" class="fotoMembro"></img>
                         <div className="redesSociaisMembro">
                             <a href="https://instagram.com" target="_blank" rel="noreferrer"><img src={Instagram} alt="Instagram"></img></a>
                             <a href="https://twitter.com/" target="_blank" rel="noreferrer"><img src={Twitter} alt="Twitter"></img></a>
@@ -204,16 +226,18 @@ const Membros = () => {
                     </div>
                     <div className="infoMembro">
                         <div className="nomeMembro"><h1>Marcelo Ian Rezende Menezes</h1>
-                            <span class="textoMembro">Entrou em 2021</span>
+                            <span class="textoMembro">
+                                Colaborador<br />
+                                Entrou em 2021
+                            </span>
                         </div>
-                        <h2>Membro</h2>
                     </div>
                 </div>
             </div>
             <div className="m-5 bd-highlight col">
                 <div className="container-col">
                     <div className="membro">
-                        <img src="https://images.generated.photos/KO9_GA3oFNX2urY2Vyqmn6TDRwPEkMpMWA2QRtWSXPg/rs:fit:256:256/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA4MTIxMTYuanBn.jpg" alt="Yuri" class="fotoMembro"></img>
+                        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" alt="Yuri" class="fotoMembro"></img>
                         <div className="redesSociaisMembro">
                             <a href="https://instagram.com" target="_blank" rel="noreferrer"><img src={Instagram} alt="Instagram"></img></a>
                             <a href="https://twitter.com/" target="_blank" rel="noreferrer"><img src={Twitter} alt="Twitter"></img></a>
@@ -221,9 +245,11 @@ const Membros = () => {
                     </div>
                     <div className="infoMembro">
                         <div className="nomeMembro"><h1>Yuri Medeiros da Silva Santos</h1>
-                            <span class="textoMembro">Entrou em 2021</span>
+                            <span class="textoMembro">
+                                Bolsista<br />
+                                Entrou em 2021
+                            </span>
                         </div>
-                        <h2>Membro</h2>
                     </div>
                 </div>
             </div>
